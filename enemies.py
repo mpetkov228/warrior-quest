dffc9d522ee4763e689ceee92f1aac437db8102e
mpetkov228 <mpetkov228@gmail.com>
2025-03-22 20:25:27 +0200

    Add some enemy classes

---
+++ enemies.py
@@ -0,0 +1,43 @@
+class Enemy:
+    def __init__(self, health, damage):
+        self.health = health
+        self.damage = damage
+
+    
+    def attack(self, target):
+        target.health -= self.damage
+
+
+class Ghoul(Enemy):
+    def __init__(self):
+        super().__init__(40, 5)
+
+    
+    def speak(self):
+        print("Ghoul: Me eat brains...")
+
+
+class Troll(Enemy):
+    def __init__(self):
+        super().__init__(55, 10)
+
+
+    def speak(self):
+        print("Troll: Ye'd make a fine dinner...")
+
+
+class Crocolisk(Enemy):
+    def __init__(self):
+        super().__init__(45, 8)
+    
+
+    def speak(self):
+        print("Crocolisk: Raaawr...")
+
+
+class Raptor(Enemy):
+    def __init__(self):
+        super().__init__(60, 12)
+    
+    def speak(self):
+        print("Raptor: Raaawr...")
